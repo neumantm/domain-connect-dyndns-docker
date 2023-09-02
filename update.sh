5605ee8e3716073244214a0d3ef5bce85fc39e01
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2023-09-02 21:38:38 +0200

    Always print status before updating (#3)

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+echo "Status"
+/usr/local/bin/domain-connect-dyndns status --domain "$DOMAIN"
 echo "Updating at $(date)"
 /usr/local/bin/domain-connect-dyndns update --domain "$DOMAIN"
