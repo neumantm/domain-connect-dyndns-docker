064d36ace31dbc56b5b0615e7f3f3b631559590b
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2023-09-03 10:00:36 +0200

    Log domain (#6)

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-echo "Status"
+echo "Using domain $DOMAIN"
+echo "Status:"
 /usr/local/bin/domain-connect-dyndns status --domain "$DOMAIN"
 echo "Updating at $(date)"
 /usr/local/bin/domain-connect-dyndns update --domain "$DOMAIN"
